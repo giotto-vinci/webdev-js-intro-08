4e0ece0f3eae7f1cc5201ed27aa69e4954bc8ac3
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-20 02:19:07 -0500

    Did experiments and updated tasks in readme.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let numbers = [7, 13, 47, 4, 12, 79]
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+for (let number of numbers) {
+    console.log(number);
+} 
+
+const vegetables = ["zucchini", "arugula", "green-beans", "broccoli", "turnips"]
+
+vegetables.forEach(function(vegetable, index) {
+    console.log(index + ': ' + vegetable);
+});
+/*
+vegetables.forEach(function(vegetable) {
+    console.log(vegetable);
+});
+*/
+
+/*
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] += 12;
+}
+console.log(vegetables);
+// Each unit in the array had 12 added to it.
+
+vegetables.forEach(function(vegetable, index, array) {
+    for (let i = 0; i < vegetables.length; i++) {
+        numbers[i] += 2;
+    }
+    console.log('Element: ' + vegetable + ', Total elements: ' + array.length);
+});
+// Pretty sure Idid it wrong or I can only do this with numbers because nothing was reassigned.
+*/
+
